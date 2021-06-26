4130f44b012dc5a1d959aa24b37bf04d89b519c3
Phat Doan <phat.doan@castellhealth.com>
2021-06-26 12:00:52 -0600

    Reupload files

---
+++ live_dashboard/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from datetime import datetime 
+
+class Result(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    M = 'Male'
+    F = 'Female'
+    GENDER_TYPE = [
+        (M, 'Male'),
+        (F, 'Female')
+        ]
+
+    gender = models.CharField(
+        max_length=50,
+        choices=GENDER_TYPE,
+        default=F,
+    )
+
+    age_under_18 = 'Under 18'
+    age_18_24 = '18-24'
+    age_25_30 = '25-30'
+    age_31_35 = '31-35'
+    age_36_40 = '36-40'
+    age_41_45 = '41-45'
+    age_46_50 = '46-50'
+    age_51_55 = '51-55'
+    age_56_60 = '56-60'
+    age_51_55 = '51-55'
+    age_61_65 = '61-65'
+    age_66_plus = '66+'
+
+    AGE_GROUP = [
+        (age_under_18, 'Under 18'),
+        (age_18_24, '18-24'),
+        (age_25_30, '25-30'),
+        (age_31_35, '31-35'),
+        (age_36_40, '36-40'),
+        (age_41_45, '41-45'),
+        (age_46_50, '46-50'),
+        (age_51_55, '51-55'),
+        (age_56_60, '56-60'),
+        (age_51_55, '51-55'),
+        (age_66_plus, '66+')
+        ]
+
+    age_group = models.CharField(
+        max_length=50,
+        choices=AGE_GROUP,
+        default=age_18_24,
+    )
+    
+    minutes = models.SmallIntegerField()
+    seconds = models.SmallIntegerField()
+    miliseconds = models.SmallIntegerField(null=True, blank=True)
+    submission_date = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(null=True, blank=True)
+    
+    run_photo = models.ImageField(upload_to='media', blank=True)
+
+    def __str__(self):
+        return self.first_name + self.last_name
---
+++ live_dashboard/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from .models import Result
+from .forms import ResultForm
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def submit(request):
+    if request.method == 'GET':
+        return render(request, 'submit.html', {'form':ResultForm()})
+    else:
+        try:
+            form = ResultForm(request.POST)
+            newresult = form.save()
+            return redirect('home')
+        except ValueError:
+            return render(request, 'submit.html', {'form':ResultForm(), 'error':'Bad data passed in. Try again.'})
+
+def overall(request):
+    results = Result.objects.order_by('minutes','seconds')
+
+    return render(request, 'overall.html',{'results':results})
+
+def male_18_24(request):
+    results = Result.objects.filter(age_group='18-24', gender='Male').order_by('minutes','seconds')
+
+    return render(request, 'male_18_24.html',{'results':results})
